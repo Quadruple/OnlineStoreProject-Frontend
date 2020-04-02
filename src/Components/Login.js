f496b44ca405549287a4f8f5e78b3cadb11f2e4b
atakanataman <atakanataman@sabanciuniv.edu>
2020-04-02 15:23:51 +0300

    Initial commit for frontend repo

---
+++ src/Components/Login.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import '../StyleSheets/main.css'
+import '../StyleSheets/util.css'
+
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleFormSubmit = (event) => {
+        alert(`${this.state.username} ${this.state.password}`)
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <div class="limiter">
+                <div class="container-login100">
+                    <div class="wrap-login100">
+                        <div class="login100-pic js-tilt" data-tilt>
+                            <img src={require("../images/img-01.png")} alt="IMG"></img>
+                        </div>
+
+                        <form class="login100-form validate-form" onSubmit={this.handleFormSubmit}>
+                            <span class="login100-form-title">
+                                Member Login
+					        </span>
+
+                            <div class="wrap-input100 validate-input" >
+                                <input class="input100" type="text" name="email" placeholder="Email" onChange={this.handleUsernameChange}></input>
+                                <span class="focus-input100"></span>
+                                <span class="symbol-input100">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                </span>
+                            </div>
+
+                            <div class="wrap-input100 validate-input" >
+                                <input class="input100" type="password" name="pass" placeholder="Password" onChange={this.handlePasswordChange}></input>
+                                <span class="focus-input100"></span>
+                                <span class="symbol-input100">
+                                    <i class="fa fa-lock" aria-hidden="true"></i>
+                                </span>
+                            </div>
+
+                            <div class="container-login100-form-btn">
+                                <button type="submit" class="login100-form-btn">
+                                    Login
+						        </button>
+                            </div>
+
+                            <div class="text-center p-t-12">
+                                <span class="txt1">
+                                    Forgot 
+						        </span>
+                                <a class="txt2" href="#">
+                                    Username / Password?
+						        </a>
+                            </div>
+
+                            <div class="text-center p-t-136">
+                                <a class="txt2" href="#">
+                                    Create your Account
+							        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
