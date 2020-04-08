5d0fac61eefb1c2f486b780ae6533381738a64e4
atakanataman <atakanataman@sabanciuniv.edu>
2020-04-08 16:15:58 +0300

    Routing Implemented to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login></Login>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
