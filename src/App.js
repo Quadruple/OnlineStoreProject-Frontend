5d243c96a6c3e171a9019b0de215672ee9652a16
berkuzalp <berkuzalp@sabanciuniv.edu>
2020-04-14 08:46:21 +0300

    signup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login'
+import Sign_up from './Components/Sign-up'
 import Home_Page from './Components/Home_Page';
 
 function App() {
   return (
     <div className="App">
-      <Home_Page></Home_Page>
+      <Login></Login>
     </div>
   );
 }
